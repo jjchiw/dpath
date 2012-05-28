180aecb45eb2ff8911ea576f72e4c8bcf6fbd66e
Juan J. Chiw <jjchiw@gmail.com>
2012-05-28 20:45:25 +0200

    renaming .js files to .html

--- DPath/Content/js/specific/path.edit.js
+++ DPath/Content/js/specific/path.edit.js
@@ -4,7 +4,7 @@
 		$.post(url, { "name": $("#goal-name").val() }, function (data) {
 
 			//data is the path object
-			$.get("/Content/js/templates/goal.add.mustache.js", function (template) {
+			$.get("/Content/js/templates/goal.add.mustache.html", function (template) {
 				var template = Mustache.render(template, data);
 				$("#goals-list").append(template);
 				$("#goal-name").val("");
