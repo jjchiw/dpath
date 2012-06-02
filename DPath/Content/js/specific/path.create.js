80fd6975a66e6a0478b7071645a0e37a6c4417dc
Juan J. Chiw <jjchiw@gmail.com>
2012-06-02 21:34:43 +0200

    Closes #1
    Didn't add an animation, just change the class of the button to disabled
    and the value to saving, after saving reverting to the original values

--- DPath/Content/js/specific/path.create.js
+++ DPath/Content/js/specific/path.create.js
@@ -12,6 +12,9 @@
 
 	$("#add-goal").click(function () {
 		var url = "/" + $("#path-id").val() + "/add-goal";
+		$(this).addClass("disabled");
+		$(this).val("saving");
+
 		$.post(url, { "name": $("#goal-name").val() }, function (data) {
 			//data is the path object
 			$.get("/Content/js/templates/goal.add.mustache.html", function (template) {
@@ -19,6 +22,9 @@
 				$("#goals-list").prepend(template);
 				$("#goal-name").val("");
 				$("#goal-name").focus();
+
+				$(this).removeClass("disabled");
+				$(this).val("add goal");
 			});
 		});
 	});
--- DPath/Content/js/specific/path.edit.js
+++ DPath/Content/js/specific/path.edit.js
@@ -1,14 +1,22 @@
 ﻿$(document).ready(function () {
 	$("#add-goal").click(function () {
 		var url = "/" + $("#path-id").val() + "/add-goal";
-		$.post(url, { "name": $("#goal-name").val() }, function (data) {
+		var addGoalButton = $(this)
+		addGoalButton.addClass("disabled");
+		addGoalButton.val("saving");
 
+		$.post(url, { "name": $("#goal-name").val() }, function (data) {
+		
+		
 			//data is the path object
 			$.get("/Content/js/templates/goal.add.mustache.html", function (template) {
 				var template = Mustache.render(template, data);
 				$("#goals-list").append(template);
 				$("#goal-name").val("");
 				$("#goal-name").focus();
+
+				addGoalButton.removeClass("disabled");
+				addGoalButton.val("add goal");
 			});
 		});
 	});
